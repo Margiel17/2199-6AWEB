2f8c238f2d8b5fc4e7bccedec426771a0f0e7d98
Margiel17 <margielesalante@gmail.com>
2024-12-18 12:14:51 +0800

    Add files via upload

---
+++ prelim_lab_data_binding/src/app/data-binding-demo/data-binding-demo.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-data-binding-demo',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './data-binding-demo.component.html',
+  styleUrl: './data-binding-demo.component.css'
+})
+export class DataBindingDemoComponent {
+  title = "My First App!"
+  description = "This is my new Angular Application"
+
+  imageURL = '../../assets/wally.jpg';
+  w = 100;
+  h = 100;
+  altText = 'Wally';
+  textColor='blue';
+  isHighlighted = true;
+  yourName = ''
+  count = 0;
+  increment() {
+    this.count++;
+  }
+  decrement() {
+    this.count--;
+  }
+}
